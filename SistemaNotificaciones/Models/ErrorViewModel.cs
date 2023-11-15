68db6413ce7eb265e98b44791fb50252da321979
Mónica González <m.e.c.g_15@hotmail.com>
2023-11-15 00:12:26 -0600

    Agregar archivos de proyecto.

---
+++ SistemaNotificaciones/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaNotificaciones.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
